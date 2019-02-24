458b7dc137f09acad64f214dcd2bae1610c71775
sherlynlobo <sherlynlobo26@gmail.com>
2019-02-23 19:42:34 -0500

    GameOver New Game Label works

--- BrickBreaker/GameOver.swift
+++ BrickBreaker/GameOver.swift
@@ -14,6 +14,11 @@ class GameOver: SKScene {
 
      public static var scoreLabel:SKLabelNode = SKLabelNode(text: "")
     
+     public static var GO:SKLabelNode = SKLabelNode(text: "Game Over")
+    
+
+        
+   
     
     override func didMove(to view: SKView) {
         
@@ -22,6 +27,18 @@ class GameOver: SKScene {
         
         GameOver.scoreLabel.text = String(score)
         
+        
+        GameOver.GO.position.x = 0
+        GameOver.GO.position.y = 0
+        
+        GameOver.GO.fontColor = UIColor.yellow
+        GameOver.GO.fontSize = 40.0
+        GameOver.GO.zPosition = 3
+        GameOver.GO.fontName = "Arial Bold"
+        GameOver.GO.name = "GO"
+        
+        addChild(GameOver.GO)
+        
     }
     
     
@@ -29,6 +46,29 @@ class GameOver: SKScene {
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         
+        
+        for touch: AnyObject in touches {
+            let pointOfTouch = touch.location(in: self)
+            let tappedNode = atPoint(pointOfTouch)
+            
+            if tappedNode.name == "GO" {
+                /*
+                let newScene = GameScene(size: self.size)
+                newScene.scaleMode = self.scaleMode
+                let transitionEffect = SKTransition.fade(withDuration: 0.5)
+                self.view!.presentScene(newScene, transition: transitionEffect)*/
+                
+                if let go = Menu(fileNamed: "GameScene")  {
+                    go.scaleMode = .aspectFill
+                    view!.presentScene(go)
+                }
+            }
+            
+        }
+ 
+       
+        
+
         
         
     }
